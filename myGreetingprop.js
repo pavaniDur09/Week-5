9ff3fc66ef77785204e145f2d31cc915b3ee81f3
pavaniDur09 <pavanidurgamcheruvu@gmail.com>
2024-12-22 19:24:25 +0000

    Create myGreetingprop.js

---
+++ myGreetingprop.js
@@ -0,0 +1,11 @@
+import './App.css';
+function GreetingElementwithProp(props){
+    const greeting = 'Hello lets start learning function component...';
+    console.log("prop is",props.msg)
+    return(
+        <div className="App">
+            <h1>{props.msg}</h1>
+        </div>
+    );
+}
+export default GreetingElementwithProp;
